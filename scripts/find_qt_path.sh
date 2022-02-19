35feb300845cf0408b9962317ccda8037ebe7492
liangyongqin <liangyongqincn@gmail.com>
2022-02-19 18:12:18 +0800

    ubuntu 20.04

--- scripts/find_qt_path.sh
+++ scripts/find_qt_path.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-QT_VER="$(ls ~/Qt/ | grep 5 -m1)"
+QT_VER="$(ls /opt/Qt5.10.0/5.10.0/gcc_64/lib | grep 5 -m1)"
 
-printf "${HOME}/Qt/${QT_VER}/gcc_64/"
+printf "/opt/Qt5.10.0/5.10.0/gcc_64/lib"
 
